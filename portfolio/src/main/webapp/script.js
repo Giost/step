72eb471c8691a70b73cda1f2741d4cf098cc0402
Stefano <stefanogioda@gmail.com>
2020-07-17 12:23:54 +0000

    Add random slide, change images path to absolute and improve slider function.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,22 +13,50 @@
 // limitations under the License.
 
 /**
-* Insert/remove the class in the element.
-*/
-function toggleClass(element, classAttribute) {
-  if (!element || typeof element.className !== "string") {
-    return;
+ * Check the validity of the parameters.
+ */
+function check(element, classAttribute, functionName) {
+  if (!element || !element.classList) {
+    throw new Error(functionName + " first argument is not valid.");
   }
   if (!classAttribute || typeof classAttribute !== "string") {
-    return;
+    throw new Error(functionName + " second argument is not a string.");
   }
-  const classes = element.className.split(" ");
-  if (classes.indexOf(classAttribute) === -1) {
-    classes.push(classAttribute);
+}
+
+/**
+ * Check if the element has the class.
+ */
+function hasClass(element, classAttribute) {
+  check(element, classAttribute, "hasClass");
+  return element.classList.contains(classAttribute);
+}
+
+/**
+ * Add the class in the element.
+ */
+function addClass(element, classAttribute) {
+  check(element, classAttribute, "addClass");
+  element.classList.add(classAttribute);
+}
+
+/**
+ * Remove the class in the element.
+ */
+function removeClass(element, classAttribute) {
+  check(element, classAttribute, "removeClass");
+  element.classList.remove(classAttribute);
+}
+
+/**
+ * Insert/remove the class in the element.
+ */
+function toggleClass(element, classAttribute) {
+  if (hasClass(element, classAttribute)) {
+    removeClass(element, classAttribute);
   } else {
-    classes.splice(classes.indexOf(classAttribute), 1);
+    addClass(element, classAttribute);
   }
-  element.className = classes.join(" ");
 }
 
 /**
@@ -37,40 +65,65 @@ function toggleClass(element, classAttribute) {
 function toggleDescription(sectionId) {
   const sectionElement = document.getElementById(sectionId);
   if (!sectionElement) {
-    return;
+    throw new Error("toggleDescription wrong sectionId.");
   }
   toggleClass(sectionElement, "collapsed");
 }
 
 /**
- * Change the slide to the current one + n
-*/
-function changeSlide(n, slider) {
-  if (!n || !Number.isInteger(n)) {
-    throw "changeSlide first argument is not an integer.";
-  }
+ * Hide the currently displayed slide and return the total number of slides
+ * and the index of the currently displayed.
+ */
+function hideAllSlides(slider) {
   let totalSlides = 0;
   let currentlyDisplayed;
   for (const elem of slider.children) {
-    const classes = elem.className.split(" ");
-    if (classes.indexOf("slide") !== -1) {
-      if (classes.indexOf("hidden") === -1) {
+    if (hasClass(elem, "slide")) {
+      if (!hasClass(elem, "hidden")) {
         currentlyDisplayed = totalSlides;
-        elem.className += " hidden";
+        addClass(elem, "hidden");
       }
       totalSlides++;
     }
   }
-  const slideToShow = slider.children[(currentlyDisplayed + n + totalSlides) % totalSlides];
-  slideToShow.className = slideToShow.className.replace("hidden", "").trim();
+  return {totalSlides, currentlyDisplayed};
 }
 
+/**
+ * Change the slide to the current one + offset.
+ */
+function changeSlide(offset, slider) {
+  if (!offset || !Number.isInteger(offset)) {
+    throw new Error("changeSlide first argument is not an integer.");
+  }
+  const {totalSlides, currentlyDisplayed} = hideAllSlides(slider);
+  const slideToShow = slider.children[(currentlyDisplayed + offset + totalSlides) % totalSlides];
+  removeClass(slideToShow, "hidden");
+}
+
+/**
+ * Change the slide to the previous one.
+ */
 function previousSlide(previousButton) {
   // The parentElement of the button is the slider container
   changeSlide(-1, previousButton.parentElement);
 }
 
+/**
+ * Change the slide to the next one.
+ */
 function nextSlide(nextButton) {
   // The parentElement of the button is the slider container
   changeSlide(1, nextButton.parentElement);
+}
+
+/**
+ * Show a random slide.
+ */
+function randomSlide(randomButton) {
+  // The nextElementSibling of the random button is the slider container
+  const slider = randomButton.nextElementSibling;
+  const {totalSlides} = hideAllSlides(slider);
+  const slideToShow = slider.children[Math.floor(Math.random() * totalSlides)];
+  removeClass(slideToShow, "hidden");
 }
